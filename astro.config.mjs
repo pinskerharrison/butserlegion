d8713f862963a2861f78b4148af2f5d39524f4d2
Harrison Pinsker <pinskerharrison@gmail.com>
2026-09-08 15:14:23 +0100

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import sitemap from '@astrojs/sitemap';
 import { redirects } from './src/data/redirects.mjs';
 
 export default defineConfig({
-  site: 'https://butserlegion.co.uk',
+  site: 'https://pinskerharrison.github.io',
+  base: '/butserlegion',
   output: 'static',
   trailingSlash: 'always',
-  integrations: [sitemap({ filter: (url) => !Object.keys(redirects).some(path => new URL(url).pathname === path) && !url.endsWith('/404/') })],
+  integrations: [
+    sitemap({
+      filter: (url) =>
+        !Object.keys(redirects).some(
+          path => new URL(url).pathname === path
+        ) && !url.endsWith('/404/')
+    })
+  ],
   redirects,
   markdown: { syntaxHighlight: false },
 });
